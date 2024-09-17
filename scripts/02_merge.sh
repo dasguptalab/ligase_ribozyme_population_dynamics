22a07cc27188caf82f74977229ff46a698eee459
ElizabethBrooks <merimaat1@gmail.com>
2024-09-16 20:30:51 -0400

    Updated merge script

--- scripts/02_merge.sh
+++ scripts/02_merge.sh
@@ -7,12 +7,12 @@
 #$ -q largemem
 
 # script to perform merging of paired end reads into single reads
-# usage: qsub 03_merge.sh
+# usage: qsub 02_merge.sh
 ## outputs_4quality15
 ## job 807481
 ## job 808607
 ## outputs_4quality20
-
+## job 817812
 ## outputs_81quality20
 
 
@@ -51,13 +51,11 @@ cd $softwarePath
 echo "Processing..."
 
 # loop through all forward and reverse reads and merge each pair into a single read
-for f1 in $inputsPath"/"*_pForward\.fq\.gz; do
-	# trim extension from current file name
-	curSample=$(echo $f1 | sed 's/_pForward\.fq\.gz//')
+for f1 in $inputsPath"/"*_pForward\.fq*; do
 	# set paired file name
-	f2=$curSample"_pReverse.fq.gz"
+	f2=$(echo $f1 | sed 's/_pForward/_pReverse/')
 	# trim to sample tag
-	sampleTag=$(basename $f1 | sed 's/_pForward\.fq\.gz//')
+	sampleTag=$(basename $f1 | sed 's/_pForward\.fq.*//')
 	# status message
 	echo "Processing $sampleTag"
 	./NGmerge -v -n 8 -1 $f1 -2 $f2 -o $mergeOut"/"$sampleTag"_stiched_reads.fq" -m 8 -p 0 -d -e 8 -l $mergeOut"/logs/"$sampleTag"_log_stitching_results.txt" -f $mergeOut"/logs/"$sampleTag"_stiched_reads_failed.fq" -j $mergeOut"/logs/"$sampleTag"_log_formatted_alignments.txt" -q 33 -u 40
