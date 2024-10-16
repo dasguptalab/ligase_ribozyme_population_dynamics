51ba26acdf24e66583e5a9fd7f61a7531390026f
ElizabethBrooks <merimaat1@gmail.com>
2024-10-16 13:55:16 -0400

    Updated inputs path

--- scripts/05_format.sh
+++ scripts/05_format.sh
@@ -8,10 +8,10 @@
 analysisType=$1
 
 # retrieve the analysis type
-analysisTag=$(grep "analysis:" ../"inputs/inputPaths_local.txt" | tr -d " " | sed "s/analysis://g")
+analysisTag=$(grep "analysis:" ../"inputs/inputPaths_HPC.txt" | tr -d " " | sed "s/analysis://g")
 
 # retrieve analysis outputs absolute path
-outputsPath=$(grep "outputs:" ../"inputs/inputPaths_local.txt" | tr -d " " | sed "s/outputs://g")
+outputsPath=$(grep "outputs:" ../"inputs/inputPaths_HPC.txt" | tr -d " " | sed "s/outputs://g")
 
 # retrieve the inputs path
 inputsPath=$outputsPath"/"$analysisType
