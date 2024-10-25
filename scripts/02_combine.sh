da5a51f36605aef47a2af158183cac4b90b66fdc
ElizabethBrooks <merimaat1@gmail.com>
2024-10-25 16:23:10 -0400

    Updated combining script

--- scripts/02_combine.sh
+++ scripts/02_combine.sh
@@ -31,26 +31,14 @@ echo "Analyzing combined data..."
 gunzip -v $inputsPath"/"*\.gz
 
 # loop over un-filtered merged reads for each run
-for f1 in $inputsPath"/"*_pForward\.fq; do
+for f1 in $inputsPath"/"*\.extendedFrags.fastq; do
 	# trim to sample tag
 	sampleTag=$(basename $f1 | sed 's/_pForward\.fq//')
 	# status message
 	echo "Processing $sampleTag ..."
 	# combine un-filtered merged,.fqiled merged, and unpaired trimmed reads
-	cat $f1 $inputsPath"/"$sampleTag"_pReverse.fq" $inputsPath"/"$sampleTag"_uForward.fq" $inputsPath"/"$sampleTag"_uReverse.fq" > $outputsPath"/"$sampleTag"_combined.fq"
+	cat $f1 $inputsPath"/"$sampleTag".notCombined_1.fastq" $inputsPath"/"$sampleTag".notCombined_2.fastq" > $outputsPath"/"$sampleTag"_combined.fq"
 done
-
-# loop through all samples
-#for f1 in $inputsPath"/"*_stiched_reads\.fq; do
-	# trim file extension
-	#sampleFile=$(echo $f1 | sed 's/\.fq//')
-	# trim to sample tag
-	#sampleTag=$(basename $f1 | sed 's/\.fq//')
-	# status message
-	#echo "Processing $sampleTag ..."
-	# combine un-filtered merged, failed merged, and unpaired trimmed reads
-	#cat $f1 $sampleFile"_failed.fq_1.fastq" $sampleFile"_failed.fq_2.fastq" > $outputsPath"/"$sampleTag"_combined.fq"
-#done
 
 # double check that there are no duplicate reads
 #for i in $outputsPath"/"*_combined\.fq; do echo $i; cat $i | awk 'NR%2==1' | cut -d' ' -f1 | sort | uniq -c | sort -n | head; done
