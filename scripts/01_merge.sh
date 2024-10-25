a6dff1d0a821b38b1dda6a29b7a1e353e39e66f0
ElizabethBrooks <merimaat1@gmail.com>
2024-10-25 16:13:22 -0400

    Adjusting merging flags

--- scripts/01_merge.sh
+++ scripts/01_merge.sh
@@ -51,7 +51,8 @@ for f1 in $inputsPath"/"*_R1_001\.fastq\.gz; do
 	# status message
 	echo "Processing $sampleTag"
 	# perform merging of paired reads
-	./flash -t 4 -o $sampleTag -d $outputsPath -r 81 $f1 $f2
+	#./flash -t 4 -o $sampleTag -d $outputsPath -r 81 $f1 $f2
+	./flash -t 4 -o $sampleTag -d $outputsPath -M 81 $f1 $f2
 	# status message
 	echo "$sampleTag processed!"
 done
