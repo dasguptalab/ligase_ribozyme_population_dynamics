efbd03da4ef18cc728dcae25ad22af183867b2e8
ElizabethBrooks <merimaat1@gmail.com>
2024-10-28 14:25:10 -0400

    Cleaned up pipeline and notes

--- scripts/07a_cluster.sh
+++ scripts/07a_cluster.sh
@@ -11,6 +11,7 @@
 # usage ex: qsub 07a_cluster.sh r8_S8_L001_formatted.fa
 # alternate usage: bash 07a_cluster.sh inputFile
 # usage ex: bash 07a_cluster.sh r8_S8_L001_formatted_above9.fa
+# usage ex: bash 07a_cluster.sh doped21-r3_S12_L001_formatted_above9.fa
 
 # load the software module
 module load bio/0724
@@ -28,7 +29,7 @@ nameTag=$(basename $inputFile | sed "s/\.fa//g" | sed "s/\./_/g")
 analysisTag=$(grep "analysis:" ../"inputs/inputPaths_HPC.txt" | tr -d " " | sed "s/analysis://g")
 
 # make a directory for the clustering outputs
-clusterOut=$outputsPath"/07_clustered"
+clusterOut=$outputsPath"/07a_clustered"
 mkdir $clusterOut
 
 # move to the new directory
