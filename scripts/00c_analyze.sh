8bfa4ff055a6dae9bbfc7f82277d29f5cd37bc00
ElizabethBrooks <merimaat1@gmail.com>
2024-10-28 01:35:09 -0400

    Adjusting pipeline inputs

--- scripts/00c_analyze.sh
+++ scripts/00c_analyze.sh
@@ -13,12 +13,12 @@
 analysisType=$1
 
 # retrieve analysis outputs absolute path
-outputsPath=$(grep "outputs:" ../"inputs/inputPaths_local.txt" | tr -d " " | sed "s/outputs://g")
+outputsPath=$(grep "outputs:" ../"inputs/inputPaths_HPC.txt" | tr -d " " | sed "s/outputs://g")
 
 # check input analysis type
 if [[ $analysisType == "raw" ]]; then
 	# retrieve raw paired reads absolute path for alignment
-	readPath=$(grep "pairedReads:" ../"inputs/inputPaths_local.txt" | tr -d " " | sed "s/pairedReads://g")
+	readPath=$(grep "pairedReads:" ../"inputs/inputPaths_HPC.txt" | tr -d " " | sed "s/pairedReads://g")
 else 
 	# set reads path
 	readPath=$outputsPath"/"$analysisType
