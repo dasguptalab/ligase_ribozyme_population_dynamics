5838e12e702136c398dab8594e34227c3d1642ea
ElizabethBrooks <merimaat1@gmail.com>
2024-11-21 22:22:01 -0500

    Updated script to count seqs

--- scripts/10_cluster_families.sh
+++ scripts/10_cluster_families.sh
@@ -10,13 +10,25 @@
 # usage ex: qsub 10_cluster_families.sh 07a_clustered r8_S8_L001
 ## job 985948
 # usage ex: qsub 10_cluster_families.sh 07a_clustered r7_S7_L001
-## job 
+## job 985958
 # usage ex: qsub 10_cluster_families.sh 07a_clustered r6_S6_L001
-## job 
+## job 985960
 # usage ex: qsub 10_cluster_families.sh 07a_clustered doped21-r3_S12_L001
-## job
+## job 985961
 # usage ex: qsub 10_cluster_families.sh 07a_clustered doped21-r2_S11_L001
-## job
+## job 985962
+# usage ex: qsub 10_cluster_families.sh 07b_clustered r8_S8_L001
+## job 985968
+# usage ex: qsub 10_cluster_families.sh 07b_clustered r7_S7_L001
+## job 985975
+# usage ex: qsub 10_cluster_families.sh 07b_clustered r6_S6_L001
+## job 985976
+# usage ex: qsub 10_cluster_families.sh 07b_clustered r5_S5_L001
+## job 985977
+# usage ex: qsub 10_cluster_families.sh 07b_clustered doped21-r3_S12_L001
+## job 985978
+# usage ex: qsub 10_cluster_families.sh 07b_clustered doped21-r2_S11_L001
+## job 985979
 
 # retrieve input file
 inputFile=$1
@@ -25,10 +37,10 @@ inputFile=$1
 inputSeqs=$2"_formatted_above9_cluster_sequences_identity_table.csv"
 
 # retrieve the analysis type
-analysisTag=$(grep "analysis:" ../"inputs/inputPaths_HPC.txt" | tr -d " " | sed "s/analysis://g")
+analysisTag=$(grep "analysis:" ../"inputs/inputPaths_local.txt" | tr -d " " | sed "s/analysis://g")
 
 # retrieve analysis outputs absolute path
-outputsPath=$(grep "outputs:" ../"inputs/inputPaths_HPC.txt" | tr -d " " | sed "s/outputs://g")
+outputsPath=$(grep "outputs:" ../"inputs/inputPaths_local.txt" | tr -d " " | sed "s/outputs://g")
 
 # retrieve the inputs path
 inputsPath=$outputsPath"/09_cluster_identity/"$inputFile
@@ -53,7 +65,7 @@ cd $tablesOut
 countsOut=$tablesOut"/"$2"_cluster_families_table.csv"
 
 # retrieve header
-header=$(head -1 $inputSeqs)
+header=$(head -1 $inputsPath"/"$inputSeqs)
 
 # add the run names to the header 
 header=$(echo $header",doped21-r1_counts,doped21-r2_counts,doped21-r3_counts,r1_counts,r2_counts,r3_counts,r4_counts,r5_counts,r6_counts,r7_counts,r8_counts")
