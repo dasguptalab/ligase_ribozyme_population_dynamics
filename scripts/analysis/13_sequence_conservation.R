8c31a82e41ec83b7b169c7cc01a1cd6d50ef95a3
ElizabethBrooks <merimaat1@gmail.com>
2025-07-15 17:48:13 -0400

    Fixed typo in overhang conservation algorithm

--- scripts/analysis/13_sequence_conservation.R
+++ scripts/analysis/13_sequence_conservation.R
@@ -278,7 +278,7 @@ for (seq_num in 1:seq_data_length) {
           mismatch_flag <- 1
         }
         # check if end of window or mismatch
-        if (window_index == complement_length | mismatch_flag == 1){ 
+        if (window_index == complement_length || mismatch_flag == 1){ 
           # store current subset length
           subset_length_list <- c(subset_length_list, subset_length)
           # reset subset length
